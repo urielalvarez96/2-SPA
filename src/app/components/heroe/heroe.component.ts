d3c5b2a308597ea0d3635f0dc00ee0375c16b941
Uriel <uriel.alvarez123@gmail.com>
2017-10-24 23:32:06 -0500

    commit

---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../servicios/heroes.service';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['../../../styles.css']
+})
+export class HeroeComponent{
+
+  heroe:any = {};
+
+  constructor( private activatedRoute:ActivatedRoute,
+              private _heroesService: HeroesService
+   ) {
+
+
+
+
+      this.activatedRoute.params.subscribe( params =>{
+        this.heroe = this._heroesService.getHeroe( params['id'] );
+      })
+
+  }
+
+
+}
